fb0c62f5fe47d623c781ff5504d06c2a9e45e895
menezesds <menezesds@hotmail.com>
2024-05-23 08:53:14 -0300

    first commit: chore- initial config. feat- create first test

---
+++ qaapispecflow/Features/getSingleUserRequest.feature
@@ -0,0 +1,8 @@
+﻿Feature: To test the get request
+
+A short summary of the feature
+
+@tag1
+Scenario: GET request testing
+	Given the user sends a get request with url as "https://reqres.in/api/users/2"
+	Then the request should be success and returnn status code 200
---
+++ qaapispecflow/StepDefinitions/HttpGetSingleUserRequestTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow.Infrastructure;
+
+namespace qaapispecflow.StepDefinitions
+{
+    [Binding]
+    public class HttpGetSingleUserRequestTest
+    {
+        HttpClient httpClient;
+        HttpResponseMessage response;
+        string responseBody;
+        private readonly ISpecFlowOutputHelper _specFlowOutputHelper;
+        public HttpGetSingleUserRequestTest(ISpecFlowOutputHelper _specFlowOutputHelper)
+        {
+            httpClient = new HttpClient();
+            this._specFlowOutputHelper = _specFlowOutputHelper;
+        }
+
+        [Given(@"the user sends a get request with url as ""([^""]*)""")]
+        public async Task WhenTheUserSendsAGetRequestWithUrlAs(string url)
+        {
+            response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            responseBody = await response.Content.ReadAsStringAsync();
+            _specFlowOutputHelper.WriteLine(responseBody);
+        }
+
+        [Then(@"the request should be success and returnn status code (.*)")]
+        public void ThenTheRequestShouldBeSuccessAndReturnnStatusCode(int statusCode)
+        {
+            Assert.True(response.IsSuccessStatusCode);
+            Assert.Equal(statusCode, (int)response.StatusCode);
+        }
+
+    }
+}
